62f553f1aff78be07c8b5806790a652c5941c1e3
rasheed <thcmofskot@gmail.com>
2024-04-17 22:35:18 +0100

    Use context managers for file handling and correct UTC time handling

--- gen.py
+++ gen.py
@@ -4,40 +4,33 @@ import time
 import socket
 import platform
 
-timestr = time.strftime("%Y-%m-%d - %H:%M:%S UTC")
+with open("./README.md", "w") as f:
+    f.write(f'''
 
+    <p align="left"> <img src="https://komarev.com/ghpvc/?username=xtenzq&label=Profile%20views&color=0e75b6&style=plastic" alt="xtenzq" /> </p>  
 
+    <p> <a href="https://twitter.com/xtenzq" target="blank"><img align="center" src="https://iconape.com/wp-content/png_logo_vector/drone.png" height="50px" width="50px" />  </a>  .  .
+    <a href="https://youtube.com" target="blank"><img align="center" src="https://iconape.com/wp-content/files/cm/286303/svg/youtube-icon-logo-logo-icon-png-svg.png" height="50px" width="70px" />  </a>  .  .
+    <a href="https://twitter.com/xtenzq" target="blank"><img align="center" src="https://image.flaticon.com/icons/png/128/1409/1409937.png" height="50px" width="50px" /></a> </p>  
+    <p align="left"> <img src="https://www.pngarts.com/files/12/Blue-Discord-Logo-Icon-PNG-Picture.png" height="50%" width="50%"  /> </p>  
 
-f = open("./README.md", "w")
+    ```bash
 
-f.write(f'''
+    {time.strftime("%Y-%m-%d - %H:%M:%S UTC", time.gmtime())}
 
- <p align="left"> <img src="https://komarev.com/ghpvc/?username=xtenzq&label=Profile%20views&color=0e75b6&style=plastic" alt="xtenzq" /> </p>  
+    ```
 
- <p> <a href="https://twitter.com/xtenzq" target="blank"><img align="center" src="https://iconape.com/wp-content/png_logo_vector/drone.png" height="50px" width="50px" />  </a>  .  .
- <a href="https://youtube.com" target="blank"><img align="center" src="https://iconape.com/wp-content/files/cm/286303/svg/youtube-icon-logo-logo-icon-png-svg.png" height="50px" width="70px" />  </a>  .  .
- <a href="https://twitter.com/xtenzq" target="blank"><img align="center" src="https://image.flaticon.com/icons/png/128/1409/1409937.png" height="50px" width="50px" /></a> </p>  
-  <p align="left"> <img src="https://www.pngarts.com/files/12/Blue-Discord-Logo-Icon-PNG-Picture.png" height="50%" width="50%"  /> </p>  
+    ```bash
 
+    Host Name : {socket.gethostname()}
 
- ```bash
+    platform  : {platform.platform()}
 
- {timestr}
+    Ip Local  : {socket.gethostbyname(socket.gethostname())}
 
- ```
+    ```
 
-
- ```bash
-
- Host Name : {socket.gethostname()}
-
- platform  : {platform.platform()}
-
- Ip Local  : {socket.gethostbyname(socket.gethostname())}
-
- ```
-
-
+    ''')
 
  ''')
 
